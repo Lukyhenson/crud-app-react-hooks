4acdd3e120e05f775c66dd78ead57af6d20ab0cd
Tauan Tathiell <lukyhenson@live.com>
2019-01-03 10:52:48 -0200

    Updates

---
+++ precache-manifest.0be6e9d0b3d6e21049e5f7caf47af03d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50f5209078ba682fedc5",
+    "url": "/Lukyhenson/crud-app-react-hooks/static/js/runtime~main.50f52090.js"
+  },
+  {
+    "revision": "2efc8da6926fa96e6ed8",
+    "url": "/Lukyhenson/crud-app-react-hooks/static/js/main.2efc8da6.chunk.js"
+  },
+  {
+    "revision": "daaf546f5bda08ee30ee",
+    "url": "/Lukyhenson/crud-app-react-hooks/static/js/1.daaf546f.chunk.js"
+  },
+  {
+    "revision": "2efc8da6926fa96e6ed8",
+    "url": "/Lukyhenson/crud-app-react-hooks/static/css/main.a09cee7a.chunk.css"
+  },
+  {
+    "revision": "d46669b2690aa666d7156b0fb10e1149",
+    "url": "/Lukyhenson/crud-app-react-hooks/index.html"
+  }
+];
